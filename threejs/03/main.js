359be3edb8f66a3fbab3fbbbb08cff02ba688add
James Woo <jameswoo@dasein.edu.my>
2025-05-14 16:15:22 +0800

    hover text show image

--- threejs/03/main.js
+++ threejs/03/main.js
@@ -1,5 +1,18 @@
 import * as THREE from 'three'
 
+
+/* Cursor */
+const cursor = {
+    x: 0,
+    y: 0
+}
+
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / window.innerWidth - 0.5
+    cursor.y = -(event.clientY / window.innerHeight - 0.5)
+    console.log(cursor.x, cursor.y)
+})
+
 const canvas = document.querySelector('.webgl')
 
 const scene = new THREE.Scene()
@@ -21,4 +34,23 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+const clock = new THREE.Clock()
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    //mesh.rotation.y = elapsedTime
+
+    // Update camera
+    camera.position.x = cursor.x * 3
+    camera.position.y = cursor.y * 3
+    camera.lookAt(mesh.position)
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+tick()
